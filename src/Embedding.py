204eadbaf8d02adb40e31d1ce5e1d96a7f43af7c
Alexander Keijser <alexanderkeijser73@gmail.com>
2018-09-22 13:24:06 +0200

    Added examples and saved model

--- src/Embedding.py
+++ src/Embedding.py
@@ -9,6 +9,12 @@ class Embedding(object):
 	"""
 
 	def __init__(self, dim=256, emb_type='cbow'):
+		"""
+		Args:
+			dim:		number embedding dimensions
+			emb_type:	word2vec type 'cbow' or 'skip_gram'
+
+		"""
 		self.dim = dim
 		self.type = emb_type
 		self.model = None
@@ -16,22 +22,38 @@ class Embedding(object):
 	def train(self, DataLoader):
 		"""
 		Trains the embedding
+
+		Args:
+			DataLoader:	DataLoader object containing loaded train comments.
+
 		"""
-		# word_tokenize(sent) for sent in
 		print('Indexing training sentences...') 
+		# Extract sentences from all training comments in the DataLoader
 		sentences = [word_tokenize(sent) for comment in iter(DataLoader.train_comments.values()) \
 						for sent in sent_tokenize(comment[0].replace('<br />',''))]
-		sentences = read_sentences(DataLoader)
 		print('Training word2vec model...')
 		self.model = Word2Vec(sentences, size=self.dim, sg=1 if self.type=='skip_gram' else 0)
 		print('Finished training model.')
 
 
 def main():
-	dl = DataLoader()
-	dl.load_train_comments()
-	emb = Embedding()
-	emb.train(dl)
+	train = False
+	
+	if train:
+		# Demonstration training new model
+		dl = DataLoader()
+		dl.load_train_comments()
+		emb = Embedding()
+		emb.train(dl)
+		emb.model.save('./cbow_model')
+	else:
+
+		# Demonstration using pretrained model
+		model = Word2Vec.load("./cbow_model")
+		vector = model.wv['computer']  # numpy vector of a word
+		print('\n\nCalling model.wv[\"computer\"] yields:\n', vector)
+		sim_to_book = model.wv.most_similar(positive='book', topn=5)
+		print('\n\nCalling model.wv.most_similar(positive=\"book\", topn=5) yields:\n', sim_to_book)
 
 if __name__ == '__main__':
 	main()
