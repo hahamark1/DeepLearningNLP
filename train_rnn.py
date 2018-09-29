c1712a4082f573c4d3b1568c1f64f39ff1a2cba8
Alexander Keijser <alexanderkeijser73@gmail.com>
2018-09-29 21:59:32 +0200

    Added checkpointing, argparser, tensorboard summaries
    Does not work for test set yet

--- train_rnn.py
+++ train_rnn.py
@@ -6,9 +6,10 @@ import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 import torchvision.datasets as dsets
+import argparse
 
 from sklearn.feature_extraction.text import CountVectorizer
-from vocab import Vocab, UnkVocab
+# from vocab import Vocab, UnkVocab
 import numpy as np
 # from src.DataLoader import DataLoader
 from RNN import RNNModel
@@ -22,23 +23,8 @@ nltk.download('punkt')
 
 
 from nltk.tokenize import word_tokenize, sent_tokenize
+from tensorboardX import SummaryWriter
 
-'''
-HYPERPARAMETERS
-'''
-USE_PADDING = False
-use_LSTM = False
-input_dim = 1
-hidden_dim = 20
-layer_dim = 3  # ONLY CHANGE IS HERE FROM ONE LAYER TO TWO LAYER
-output_dim = 2
-batch_size = 16
-n_iters = 3000
-
-n_epochs = 25
-max_steps = 10
-
-learning_rate = 0.1
 
 def calc_accuracy(output, batch_targets):
     predictions = torch.argmax(output,dim=1)
@@ -46,31 +32,43 @@ def calc_accuracy(output, batch_targets):
     return float(correct_predictions.item() / len(batch_targets)) * 100
 
 
-def train(dl, num_epochs):
+def train(dl, config):
+
+    writer = SummaryWriter(config.summary_path)
+
     total_loss = 0
     word_seq_size = dl.train_data.seq_size_words
     chr_seq_size = dl.train_data.seq_size_chars
 
-    if use_LSTM:
-        model = LSTMModel(input_dim, hidden_dim, layer_dim, output_dim)
+    if config.use_LSTM:
+        model = LSTMModel(input_dim, config.hidden_dim, config.num_layers, config.output_dim)
     else:
-        model = RNNModel(word_seq_size, hidden_dim, layer_dim, output_dim)
+        model = RNNModel(word_seq_size, config.hidden_dim, config.num_layers, config.output_dim)
 
     if torch.cuda.is_available():
         model.cuda()
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.SGD(model.parameters(), lr=config.learning_rate)
 
-    for index in range(n_iters):
+        # Load checkpoint
+    if config.checkpoint:
+        checkpoint = torch.load(config.checkpoint)
+        model.load_state_dict(checkpoint['model'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        epoch = checkpoint['epoch']
+        print("Checkpoint loaded")
+
+
+    for index in range(config.n_iters):
         t1 = time.time()
         print('Starting on iteration {}'.format(index+1))
         # load new batch
-        batch_inputs_words, batch_inputs_chars, batch_targets_label, batch_targets_scores = dl.train_data.next_batch(batch_size)
-        print(batch_inputs_words.shape)
+        batch_inputs_words, batch_inputs_chars, batch_targets_label, batch_targets_scores = dl.train_data.next_batch(config.batch_size)
+        # print(batch_inputs_words.shape)
         if torch.cuda.is_available():
             batch_inputs_words, batch_inputs_chars, batch_targets_label, batch_targets_scores = batch_inputs_words.cuda(), batch_inputs_chars.cuda(), batch_targets_label.cuda(), batch_targets_scores.cuda()
-        batch_inputs_words = batch_inputs_words.t().reshape(batch_size, word_seq_size, 1)
+        batch_inputs_words = batch_inputs_words.t().reshape(config.batch_size, word_seq_size, 1)
 
         optimizer.zero_grad()
 
@@ -84,6 +82,11 @@ def train(dl, num_epochs):
 
         acc = calc_accuracy(outputs, label)
 
+        # niter = epoch*len(data_loader)+step
+        writer.add_scalar('accuracy', acc, index)
+        writer.add_scalar('loss', loss.item(), index)
+
+
         # Getting gradients w.r.t. parameters
         loss.backward()
         nn.utils.clip_grad_norm_(model.parameters(), 1)
@@ -91,15 +94,18 @@ def train(dl, num_epochs):
         # Updating parameters
         optimizer.step()
         t2 = time.time()
-        examples_per_second = batch_size/float(t2-t1)
+        examples_per_second = config.batch_size/float(t2-t1)
 
         if index % 1 == 0:
             print('[{}]\t Step {}\t Loss {} \t Examples/Sec = {:.2f},'.format(datetime.now().strftime("%Y-%m-%d %H:%M"),
                             index, total_loss, examples_per_second))
             total_loss = 0
 
-        if index % 10 == 0:
-            test(dl, index, model)
+        if index % config.save_every == 0:
+            save_checkpoint(model, optimizer)
+
+        # if index % 10 == 0:
+            # test(dl, index, model)
 
 
 def test(dl, step, model):
@@ -124,13 +130,33 @@ def test(dl, step, model):
     acc = calc_accuracy(outputs, label)
 
     t2 = time.time()
-    examples_per_second = batch_size/float(t2-t1)
+    examples_per_second = config.batch_size/float(t2-t1)
 
     print('Here the test results after {} steps.\n[{}]\t Loss {} \t Acc {} \t Examples/Sec = {:.2f},'.format(step, datetime.now().strftime("%Y-%m-%d %H:%M"),
                     loss.item(), acc, examples_per_second))
 
+def print_flags():
+  """
+  Prints all entries in FLAGS variable.
+  """
+  print('\n')
+  for key, value in vars(config).items():
+    print(key + ' : ' + str(value))      
+  print('\n')
 
-def main():
+def save_checkpoint(model, optimizer, filename='checkpoints_dl4nlt/checkpoint.pth.tar'):
+    if not os.path.exists('./checkpoints_dl4nlt'):
+        os.mkdir('./checkpoints_dl4nlt')
+    filename = "checkpoints_dl4nlt/checkpoint_{}.pth.tar".format(datetime.now().strftime("%d_%m_%H_%M"),)
+    checkpoint = {
+        # 'epoch': epoch + 1,
+        'model': model.state_dict(),
+        'optimizer' : optimizer.state_dict(),
+        }
+    torch.save(checkpoint, filename)    
+
+
+def main(config):
     limit = 16
     data_loader_filename = 'data/dataloader_{}.p'.format(limit)
 
@@ -144,10 +170,42 @@ def main():
         with open(data_loader_filename, 'wb') as wf:
             pickle.dump(dl, wf)
 
-    num_epochs = n_iters / (dl.train_data.num_examples / batch_size)
+    num_epochs = config.n_iters / (dl.train_data.num_examples / config.batch_size)
     num_epochs = int(num_epochs)
 
-    train(dl, num_epochs)
+    train(dl, config)
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+
+
+    # Parse training configuration
+    parser = argparse.ArgumentParser()
+
+    # Model params
+    parser.add_argument('--use_LSTM', type=bool, default=False, help='To use an LSTM instead of RNN')
+    parser.add_argument('--hidden_dim', type=int, default=20, help='Number of hidden units in the LSTM')
+    parser.add_argument('--num_layers', type=int, default=3, help='Number of stacked RNN/LSTM layers in the model')
+    parser.add_argument('--output_dim', type=int, default=2, help='Output dimension of the model')
+    parser.add_argument('--input_dim', type=int, default=1, help='Input dimension of the model')
+
+    # Training params
+    parser.add_argument('--use_padding', type=bool, default=False, help='To use padding on input sentences.')
+    parser.add_argument('--batch_size', type=int, default=16, help='Number of examples to process in a batch')
+    parser.add_argument('--learning_rate', type=float, default=0.1, help='Learning rate')
+    parser.add_argument('--num_epochs', type=int, default=25, help='Number of epochs')
+    parser.add_argument('--n_iters', type=int, default=3000, help='Number of training steps')
+    parser.add_argument('--max_norm', type=float, default=5.0, help='--')
+
+    # Misc params
+    parser.add_argument('--summary_path', type=str, default="./summaries_dl4nlt/", help='Output path for summaries')
+    parser.add_argument('--print_every', type=int, default=5, help='How often to print training progress')
+    parser.add_argument('--sample_every', type=int, default=100, help='How often to sample from the model')
+    parser.add_argument('--save_every', type=int, default=100, help='How often to save checkpoint')
+    parser.add_argument('--checkpoint', type=str, default=None, help='Path to checkpoint file')
+
+    config = parser.parse_args()
+
+    print_flags()
+
+    # Train the model
+    main(config)
