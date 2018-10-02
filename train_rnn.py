8ada3d37f809ef0a72bba44174b78aa4a89f29d7
Alexander Keijser <alexanderkeijser73@gmail.com>
2018-10-02 23:34:25 +0200

    bla

--- train_rnn.py
+++ train_rnn.py
@@ -181,7 +181,7 @@ def evaluate(dl, config):
     chr_seq_size = dl.train_data.seq_size_chars
 
     if config.use_LSTM:
-        model = LSTMModel(input_dim, config.hidden_dim, config.num_layers, config.output_dim)
+        model = LSTMModel(word_seq_size, config.hidden_dim, config.num_layers, config.output_dim)
     else:
         model = RNNModel(word_seq_size, config.hidden_dim, config.num_layers, config.output_dim)
 
