9434f8ac25e9317109d19f67a591577135ac680d
Alexander Keijser <alexanderkeijser73@gmail.com>
2018-10-04 11:56:28 +0200

    Edited bash scripts for previous fix

--- run_LSTM.sh
+++ run_LSTM.sh
@@ -3,4 +3,4 @@
 #PBS -lnodes=1:ppn=12
 #PBS -lmem=250GB
 
-python $HOME/DeepLearningNLP/train_rnn.py --num_layers 1 --use_LSTM true --test_size 200 --save_every 400 --test_every 25 --print_every 25 --n_iters 20000 --data_path $HOME/DeepLearningNLP/data --summary_path $HOME/DeepLearningNLP/summaries_dl4nlt_lstm --checkpoint_path $HOME/DeepLearningNLP/checkpoints_dl4nlt_lstm > $HOME/DeepLearningNLP/final_lstm.txt
+python $HOME/DeepLearningNLP/train_rnn.py --num_layers 1 --use_LSTM 1 --test_size 200 --save_every 400 --test_every 25 --print_every 25 --n_iters 20000 --data_path $HOME/DeepLearningNLP/data --summary_path $HOME/DeepLearningNLP/summaries_dl4nlt_lstm --checkpoint_path $HOME/DeepLearningNLP/checkpoints_dl4nlt_lstm > $HOME/DeepLearningNLP/final_lstm.txt
--- run_RNN.sh
+++ run_RNN.sh
@@ -3,4 +3,4 @@
 #PBS -lnodes=1:ppn=12
 #PBS -lmem=250GB
 
-python $HOME/DeepLearningNLP/train_rnn.py --num_layers 1 --use_LSTM false --test_size 200 --save_every 400 --test_every 25 --print_every 25 --n_iters 20000 --data_path $HOME/DeepLearningNLP/data --summary_path $HOME/DeepLearningNLP/summaries_dl4nlt_rnn --checkpoint_path $HOME/DeepLearningNLP/checkpoints_dl4nlt_rnn > $HOME/DeepLearningNLP/final_rnn.txt
+python $HOME/DeepLearningNLP/train_rnn.py --num_layers 1 --use_LSTM 0 --test_size 200 --save_every 400 --test_every 25 --print_every 25 --n_iters 20000 --data_path $HOME/DeepLearningNLP/data --summary_path $HOME/DeepLearningNLP/summaries_dl4nlt_rnn --checkpoint_path $HOME/DeepLearningNLP/checkpoints_dl4nlt_rnn > $HOME/DeepLearningNLP/final_rnn.txt
